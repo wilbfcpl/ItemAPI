a10604f0072a374cbab575f1fe041100c60ab88c
wilbfcpl <89823326+wilbfcpl@users.noreply.github.com>
2022-04-26 11:03:33 -0400

    Latest Version of UpdateItem.pl using Parallel:ForkManager to request ten updates at a time.

--- UpdateItem.pl
+++ UpdateItem.pl
@@ -9,6 +9,7 @@ use strict;
 use diagnostics;
 use integer;
 use Time::HiRes qw( gettimeofday tv_interval);;
+use Parallel::ForkManager;
 use Data::Dumper;
 use say;
 
@@ -18,11 +19,17 @@ use XML::Compile::SOAP11;      # use SOAP version 1.1
 use XML::Compile::Transport::SOAPHTTP;
 use Getopt::Std;
 
-use constant API_CHUNK_SIZE => 5;
+use constant API_CHUNK_SIZE => 10;
+#my $max_procs = API_CHUNK_SIZE;
+
+
+
+my $t0;
+my $elapsed;
 
 #Command line input variable handling
 our ($opt_d,$opt_r,$opt_x, $opt_i);
-getopts('drx:i');
+getopts('grx:i');
 
 if (defined $opt_d) {
    use Log::Report mode=>'DEBUG';
@@ -34,29 +41,33 @@ my $local_filename=$0;
                   
 
 # Results and trace from XML::Compile::WSDL et al.
-my $result ;
-my $trace;
+my %trace;
+my %results;
+
 my %ItemRec;
 my %UpdateItemRequest;
 
 %ItemRec = (
          #itemid=> $itemid,
          # bid=> $bid ,
-        CallNumber=>'',
-        Modifiers=> {
-        DebugMode=>1,
-        ReportMode=>0,}
+        CallNumber=>''   
          );
 
 %UpdateItemRequest = (
  ItemID => '',
- Item => \%ItemRec
+ Item => \%ItemRec,
+     Modifiers=> {
+        DebugMode=>1,
+        ReportMode=>0,}
              );
 
 
 my $wsdlfile = 'ItemAPI.wsdl';
 
 my $wsdl = XML::Compile::WSDL11->new($wsdlfile);
+my $trans = XML::Compile::Transport::SOAPHTTP
+    ->new(timeout => 500, address => $wsdl->endPoint);
+$wsdl->compileCalls(transport => $trans);
 
 unless (defined $wsdl)
 {
@@ -100,47 +111,116 @@ unless ((defined $call1) && (defined $call2) && (defined $call3) && ( defined $c
   
   #[$local_filename" . ":" . __LINE__ . "]DBI Call lapsed time $elapsed."
   my $num_chunks = $nr/API_CHUNK_SIZE;
-  my $leftovers = $nr%API_CHUNK_SIZE;
+  my $mods = $nr%API_CHUNK_SIZE;
   
-  say " [$local_filename" . ":" . __LINE__ . "]Linecount " . $nr . " Chunk Size " . API_CHUNK_SIZE . " Blocks " . $num_chunks . " Mod " . $leftovers;
+  say " [$local_filename" . ":" . __LINE__ . "]Linecount " . $nr . " Burst Size " . API_CHUNK_SIZE . " Bursts " . $num_chunks . " Mod " . $mods;
+  my (@items,@bids,@old_callnumbers,@new_callnumbers) ;
+  my $pid;
+  my (@result2,@trace2);
   
+ 
+
   # Read the input file and ignore the first line having column headings
   $_ = <>;
   chomp;
-     
+  
+#my $pm =  new Parallel::ForkManager(API_CHUNK_SIZE);
+
+## End subroutine for each of the forked processes.
+## Save results for examination by parent process
+#  $pm->run_on_finish( sub {
+#			my ($pid, $exit_code, $ident, $exit_signal, $core_dump, $data_structure_reference) = @_;
+#
+#    # my $q = $data_structure_reference->{input};
+#
+#    say "Ident: $ident PID: $pid exit code: $exit_code" ;
+#  # $results{$q} = $data_structure_reference->{result};
+#  # $trace{$q} = $data_structure_reference->{trace};
+#
+#  # if ((defined $opt_d) &&  (defined  $trace{$q})) {
+#  #  if ($trace{$q}->errors) {
+#  #    $trace{$q}->printErrors;
+#  #    } 
+#  # } 
+#});
+  
+  
   for my $current_block (1..$num_chunks)
   {
-   say " [$local_filename" . ":" . __LINE__ . "]Block $current_block";
-   for my $current_line (1..API_CHUNK_SIZE)
+   say " [$local_filename" . ":" . __LINE__ . "]Burst $current_block";
+   
+   #time the operation length in seconds
+   #$t0 = [gettimeofday];
+   
+   for my $current_line (0..API_CHUNK_SIZE-1)
    {
     chomp ;
-    #say " [$local_filename" . ":" . __LINE__ . "]Line $current_line";
+       #say " [$local_filename" . ":" . __LINE__ . "]Current line " . ($current_block * API_CHUNK_SIZE + $current_line + 1);
+
     $_ = <>;
-    my ($item, $bid, $old_callnumber, $new_callnumber ) = split(/,/);
-    say  "[$local_filename" . ":" . __LINE__ . "]Item $item, Bid $bid, OldCN $old_callnumber, NewCN $new_callnumber";
+    ($items[$current_line], $bids[$current_line], $old_callnumbers[$current_line], $new_callnumbers[$current_line] ) = split(/,/);
    }
-   
+   my $pm =  new Parallel::ForkManager(API_CHUNK_SIZE);
+   for my $current_line (0..API_CHUNK_SIZE-1)
+   {
+
+    $pid = $pm->start and next;
+
+    $ItemRec{CallNumber} = $new_callnumbers[$current_line] ;
+    $UpdateItemRequest{ItemID}=$items[$current_line] ;
+
+    ($result2[$current_line],$trace2[$current_line]) = $call2->(%UpdateItemRequest);
+    say " [$local_filename" . ":" . __LINE__ . "]Burst $current_block proc $current_line API returned";
+    $pm->finish;
+   #$pm->finish (0, { result=>$result2[$current_line], trace=>$trace2[$current_line],input=>$current_line});
+   } 
+   say " [$local_filename" . ":" . __LINE__ . "]Burst $current_block waiting...";
+   $pm->wait_all_children;
+   say " [$local_filename" . ":" . __LINE__ . "]Burst $current_block finished...";
+   sleep 1;
   }
-  for my $leftover_line (1..$leftovers)
+
+  
+  for my $mod_line (0..$mods-1)
   {
+   
    chomp;
-   say " [$local_filename" . ":" . __LINE__ . "]Leftover Line $leftover_line";
+   say " [$local_filename" . ":" . __LINE__ . "]Mod Line " . ( $num_chunks* API_CHUNK_SIZE + $mod_line + 1);
    $_=<>;
-   my ($item, $bid, $old_callnumber, $new_callnumber ) = split(/,/);
-   say  "[$local_filename" . ":" . __LINE__ . "]Item $item, Bid $bid, OldCN $old_callnumber, NewCN $new_callnumber";
-   $ItemRec{CallNumber} = $new_callnumber ;
-   $UpdateItemRequest{ItemID}=$item ;
-   
-   my ($result2,$trace2) = $call2->(%UpdateItemRequest);
-   say Dumper($result2);
-   
-   my $MyResponseStatusCode = ($result2->{UpdateItemResponse}->{ResponseStatuses}->{cho_ResponseStatus}[0]->{ResponseStatus}->{Code});
- 
-   if ( (defined $MyResponseStatusCode) && ($MyResponseStatusCode == 0) )
-    {
-    say "[$local_filename" . ":" . __LINE__ . "]ItemAPI Success: $MyResponseStatusCode" ;
-    }     
+    ($items[$mod_line], $bids[$mod_line], $old_callnumbers[$mod_line], $new_callnumbers[$mod_line] ) = split(/,/);
   }
+  my $pm =  new Parallel::ForkManager($mods);
+  
+  for my $mod_line (0..$mods-1)
+  {
+   $pid = $pm->start and next;
+   say("[$local_filename" . ":" . __LINE__ . "]Parallel Fork Mod proc $mod_line");
+   $ItemRec{CallNumber} = $new_callnumbers[$mod_line] ;
+   $UpdateItemRequest{ItemID}=$items[$mod_line] ;
+   ($result2[$mod_line],$trace2[$mod_line]) = $call2->(%UpdateItemRequest);
+   say " [$local_filename" . ":" . __LINE__ . "]Mod proc $mod_line API returned";
+   $pm->finish;
+   #$pm->finish (0, { result=>$result2[$mod_lsay " [$local_filename" . ":" . __LINE__ . "]current_line waitingine], trace=>$trace2[$mod_line],input=>$mod_line});
+   
+  }
+  say " [$local_filename" . ":" . __LINE__ . "]mod_line waiting...";
+  $pm->wait_all_children;
+  say " [$local_filename" . ":" . __LINE__ . "]mod_line finished";
+  #$elapsed = tv_interval ($t0) ;
+  #say ("[$local_filename" . ":" . __LINE__ . "]Parallel Fork Call lapsed time $elapsed.");
+
+  #  print Dumper \%results;
+ 
+   
+   
+   #
+   #my $MyResponseStatusCode = ($result2->{UpdateItemResponse}->{ResponseStatuses}->{cho_ResponseStatus}[0]->{ResponseStatus}->{Code});
+   #
+   #if ( (defined $MyResponseStatusCode) && ($MyResponseStatusCode == 0) )
+   # {
+   # say "[$local_filename" . ":" . __LINE__ . "]ItemAPI Success: $MyResponseStatusCode" ;
+   # }     
+  
   
 
      
