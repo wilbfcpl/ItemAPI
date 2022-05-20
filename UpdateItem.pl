e2c9bb505e9a73fc8a0ccb1d7d728b900370a09f
wilbfcpl <89823326+wilbfcpl@users.noreply.github.com>
2022-05-20 08:30:49 -0400

    Basecamp Update of the project. Fix for incorrect curly brace location in UpdateItem.pl - causes app to hang though the compiler doesn't care. Added filters to generate files from SQL Developer CSV export. newcn.pl generates call number output  ER <author> from input E <author> -  BEGINNING TO READ oldcn.pl generates a call number output E <author> - BEGINNING TO READ from input ER <author>

--- UpdateItem.pl
+++ UpdateItem.pl
@@ -2,13 +2,21 @@
 # Created: March 7, 2022
 # Version: 0.01
 #
-# Usage: perl [-d] [-r ] [-x] [-i] UpdateItem.pl item_file.csv
-# -d Debug/verbose -r read only , no update, -i in
+# Usage: perl [-g] [-x] [-i] UpdateItem.pl item_file.csv
+# -g Debug/verbose -q quiet
+#
+# Expects Input csv file
+# itemid,bid,old call number, new call number
+# 
+# Example First Two Lines of an Input File:
+#
+# ITEM,BID,OLDCN,NEWCN,TITLE,AUTHOR,BRANCHCODE,LOCCODE
+# 21982030026524,67393,E TRAN - BEGINNING TO READ,ER TRAN,Transformers : Training day : hunt for the Decepticons /,"Teitelbaum, Michael",CBA,EPRDR
 
 use strict;
 use diagnostics;
 use integer;
-use Time::HiRes qw( gettimeofday tv_interval);;
+use Time::HiRes qw( gettimeofday tv_interval);
 use Parallel::ForkManager;
 use Data::Dumper;
 use say;
@@ -20,25 +28,30 @@ use XML::Compile::Transport::SOAPHTTP;
 use Getopt::Std;
 
 use constant API_CHUNK_SIZE => 10;
-#my $max_procs = API_CHUNK_SIZE;
-
-
-
-my $t0;
-my $elapsed;
 
 #Command line input variable handling
-our ($opt_d,$opt_r,$opt_x, $opt_i);
-getopts('grx:i');
-
-if (defined $opt_d) {
-   use Log::Report mode=>'DEBUG';
-}
-
+our ($opt_g,$opt_x,$opt_q);
+getopts('gx:q');
 
 my $local_filename=$0;
  $local_filename =~ s/.+\\([A-z]+.pl)/$1/;
-                  
+       
+use if defined $opt_g, "Log::Report", mode=>'DEBUG';
+   
+if ( defined($opt_g) ) {
+     say "[$local_filename" . ":" . __LINE__ . "]Debug Mode $opt_g." ;
+}
+
+my $quiet_mode = 0;
+
+if ( defined($opt_q)) {
+     $quiet_mode =1 ;
+     say "[$local_filename" . ":" . __LINE__ . "]Quiet Mode $quiet_mode." ;
+}
+
+#Time::HiRes qw( gettimeofday tv_interval) related variables
+my $t0;
+my $elapsed;
 
 # Results and trace from XML::Compile::WSDL et al.
 my %trace;
@@ -61,7 +74,6 @@ my %UpdateItemRequest;
         ReportMode=>0,}
              );
 
-
 my $wsdlfile = 'ItemAPI.wsdl';
 
 my $wsdl = XML::Compile::WSDL11->new($wsdlfile);
@@ -70,8 +82,7 @@ my $trans = XML::Compile::Transport::SOAPHTTP
 $wsdl->compileCalls(transport => $trans);
 
 unless (defined $wsdl)
-{
-    die "[$local_filename" . ":" . __LINE__ . "]Failed XML::Compile call\n" ;
+{  die "[$local_filename" . ":" . __LINE__ . "]Failed XML::Compile call\n" ;
 }
            
 my $call1 = $wsdl->compileClient('GetItemList');
@@ -80,148 +91,158 @@ my $call3 = $wsdl->compileClient('DeleteItem');
 my $call4 = $wsdl->compileClient('GetItemNotes');
 my $call5 = $wsdl->compileClient('UpdateItemNote');
 
-
-
 unless ((defined $call1) && (defined $call2) && (defined $call3) && ( defined $call4)  && defined($call5))
   { die "[$local_filename" . ":" . __LINE__ . "] SOAP/WSDL Error $wsdl $call1, $call2 $call3 $call4 $call5 \n" ;}
 
- #[TODO] group API update calls into bursts of five
-  say "Argv0 $ARGV[0]" ;
-  my $nr ;
-  
-  
-  open(FILEHANDLE, "< $ARGV[0]") or die "[$local_filename" . ":" . __LINE__ . "]Unable to Open $ARGV[0]";
-       
-   while (<FILEHANDLE>) {
-    $nr += 1;
-   }
-   #Ignore first line with labels
-   $nr -= 1; 
-   seek FILEHANDLE, 0, 0;
+  say "[$local_filename" . ":" . __LINE__ . "] ARGV[0] $ARGV[0] " ;
  
- #[TODO] determine why backtick doesn't work
- 
-  #my $nr = qx/dir/ ;
-  #if ($? != 0) {
-  #  die "shell returned $?";
-  #}
-  #
-  #$nr =~ s/.+ ([0-9]+).*/$1/;
-  #
+# Use wc -l to get line count of input file $ARGV[0]
+  my $nr = qx/wc -l $ARGV[0]/ ;
+  if ($? != 0) {
+    die "[$local_filename" . ":" . __LINE__ . "]shell returned $?";
+  }
+  
+  $nr =~ s/^([0-9]+).*/$1/;
+  chomp($nr);
   
   #[$local_filename" . ":" . __LINE__ . "]DBI Call lapsed time $elapsed."
   my $num_chunks = $nr/API_CHUNK_SIZE;
   my $mods = $nr%API_CHUNK_SIZE;
   
-  say " [$local_filename" . ":" . __LINE__ . "]Linecount " . $nr . " Burst Size " . API_CHUNK_SIZE . " Bursts " . $num_chunks . " Mod " . $mods;
+  say "[$local_filename" . ":" . __LINE__ . "]Linecount " . $nr . " Burst Size " . API_CHUNK_SIZE . " Bursts " . $num_chunks . " Mod " . $mods;
   my (@items,@bids,@old_callnumbers,@new_callnumbers) ;
   my $pid;
   my (@result2,@trace2);
   
- 
-
   # Read the input file and ignore the first line having column headings
   $_ = <>;
   chomp;
   
-#my $pm =  new Parallel::ForkManager(API_CHUNK_SIZE);
+  my $pm =  new Parallel::ForkManager(API_CHUNK_SIZE*2);
 
-## End subroutine for each of the forked processes.
-## Save results for examination by parent process
-#  $pm->run_on_finish( sub {
-#			my ($pid, $exit_code, $ident, $exit_signal, $core_dump, $data_structure_reference) = @_;
-#
-#    # my $q = $data_structure_reference->{input};
-#
-#    say "Ident: $ident PID: $pid exit code: $exit_code" ;
-#  # $results{$q} = $data_structure_reference->{result};
-#  # $trace{$q} = $data_structure_reference->{trace};
-#
-#  # if ((defined $opt_d) &&  (defined  $trace{$q})) {
-#  #  if ($trace{$q}->errors) {
-#  #    $trace{$q}->printErrors;
-#  #    } 
-#  # } 
-#});
-  
-  
-  for my $current_block (1..$num_chunks)
-  {
-   say " [$local_filename" . ":" . __LINE__ . "]Burst $current_block";
-   
-   #time the operation length in seconds
-   #$t0 = [gettimeofday];
-   
-   for my $current_line (0..API_CHUNK_SIZE-1)
+# End subroutine for each of the forked processes.
+# Save result, trace for examination by parent process
+# Debug mode only
+if ( defined($opt_g) ) {
+     say "[$local_filename" . ":" . __LINE__ . "]Debug Mode $opt_g run_on_finish" ;
+
+  $pm->run_on_finish( sub {
+   my ($pid, $exit_code, $ident, $exit_signal, $core_dump, $data_structure_reference) = @_;
+   if (defined ($data_structure_reference))
    {
-    chomp ;
-       #say " [$local_filename" . ":" . __LINE__ . "]Current line " . ($current_block * API_CHUNK_SIZE + $current_line + 1);
-
-    $_ = <>;
-    ($items[$current_line], $bids[$current_line], $old_callnumbers[$current_line], $new_callnumbers[$current_line] ) = split(/,/);
+    my $entry=$data_structure_reference->{entry_line};
+    say "[$local_filename" . ":" . __LINE__ . "]Mod Process input " . $entry;
+    #say "[$local_filename" . ":" . __LINE__ . "]Dumper " . Dumper $data_structure_reference->{result} ;
+    $results{$entry} =  ${$data_structure_reference->{result}};
+    say "[$local_filename" . ":" . __LINE__ . "]Dumper " . Dumper $results{$entry};
+    #say "[$local_filename" . ":" . __LINE__ . "]Mod Process result " . $results{$entry};
+    # print Dumper $results{$entry};
+    #  $trace{$entry} =    ${${$data_structure_reference}->{tracer}};
    }
-   my $pm =  new Parallel::ForkManager(API_CHUNK_SIZE);
-   for my $current_line (0..API_CHUNK_SIZE-1)
-   {
-
-    $pid = $pm->start and next;
-
-    $ItemRec{CallNumber} = $new_callnumbers[$current_line] ;
-    $UpdateItemRequest{ItemID}=$items[$current_line] ;
-
-    ($result2[$current_line],$trace2[$current_line]) = $call2->(%UpdateItemRequest);
-    say " [$local_filename" . ":" . __LINE__ . "]Burst $current_block proc $current_line API returned";
-    $pm->finish;
-   #$pm->finish (0, { result=>$result2[$current_line], trace=>$trace2[$current_line],input=>$current_line});
-   } 
-   say " [$local_filename" . ":" . __LINE__ . "]Burst $current_block waiting...";
-   $pm->wait_all_children;
-   say " [$local_filename" . ":" . __LINE__ . "]Burst $current_block finished...";
-   sleep 1;
-  }
-
+   });
+ }
   
-  for my $mod_line (0..$mods-1)
+  for (my $current_block=0; $current_block<$num_chunks;$current_block++ )
   {
-   
-   chomp;
-   say " [$local_filename" . ":" . __LINE__ . "]Mod Line " . ( $num_chunks* API_CHUNK_SIZE + $mod_line + 1);
+    if ($quiet_mode==0) {
+    say "[$local_filename" . ":" . __LINE__ . "]Burst $current_block";
+    }
+    #time the operation length in seconds
+    #$t0 = [gettimeofday]; 
+    for my $current_line (0..API_CHUNK_SIZE-1)
+    {
+     #say " [$local_filename" . ":" . __LINE__ . "]Current line " . ($current_block * API_CHUNK_SIZE + $current_line + 1);
+     $_ = <>;
+     next if ( (not defined $_ ) || ($_=~/^ *$/) || ($_ =~/^\s*$/));
+     chomp ;
+     ($items[$current_line], $bids[$current_line], $old_callnumbers[$current_line], $new_callnumbers[$current_line] ) = split(/,/);
+    }
+    for (my $current_line=0; $current_line<API_CHUNK_SIZE; $current_line++)
+      {
+        $pid = $pm->start and next;
+    
+        $ItemRec{CallNumber} = $new_callnumbers[$current_line] ;
+        $UpdateItemRequest{ItemID}=$items[$current_line] ;
+    
+        ($result2[$current_line],$trace2[$current_line]) = $call2->(%UpdateItemRequest);
+       
+        if ( $quiet_mode == 0) {    
+          say "[$local_filename" . ":" . __LINE__ . "]Burst $current_block proc $current_line API returned";
+        }
+        my $MyResponseStatusCode = ($result2[$current_line]->{UpdateItemResponse}->{ResponseStatuses}->{cho_ResponseStatus}[0]->{ResponseStatus}->{Code});
+        #say "[$local_filename" . ":" . __LINE__ . "]MyResponseStatusCode " . $MyResponseStatusCode ;
+       
+        if ( defined($opt_g) ) {
+           say "[$local_filename" . ":" . __LINE__ . "]Debug Mode $opt_g." ;
+           $pm->finish (0,  {entry_line=>$current_line,result=>\$MyResponseStatusCode});
+           #$pm->finish (0,  {entry_line=>$current_line});
+           #$pm->finish (0,   {entry_line=>$current_line, result=>\$result2[$current_line]});
+           #$pm->finish (0, { result=>\$result2[$current_line], tracer=>\$trace2[$current_line],entry_line=>$current_line});
+        }
+        else {
+          $pm->finish;
+         }
+  
+        if ( $quiet_mode==0) {   
+         say "[$local_filename" . ":" . __LINE__ . "]Burst $current_block waiting...";
+        }
+       $pm->wait_all_children;
+       if ( $quiet_mode==0) {
+        say "[$local_filename" . ":" . __LINE__ . "]Burst $current_block finished...";
+       }
+     } # end for (my $current_line=0; $current_line<API_CHUNK_SIZE; $current_line++)
+  } # end for (my $current_block=0; $current_block<$num_chunks;$current_block++ )
+  
+  # Modulus / Remaining after dividing into bursts 
+  for ( my $mod_line=0; $mod_line<$mods; $mod_line++)
+  {
+   if ($quiet_mode==0) {
+    say "[$local_filename" . ":" . __LINE__ . "]Mod Line $mod_line";
+    }
    $_=<>;
+    next if ( (not defined $_ ) || ($_=~/^ *$/) || ($_ =~/^\s*$/));
+    chomp;
     ($items[$mod_line], $bids[$mod_line], $old_callnumbers[$mod_line], $new_callnumbers[$mod_line] ) = split(/,/);
-  }
-  my $pm =  new Parallel::ForkManager($mods);
+   }
   
-  for my $mod_line (0..$mods-1)
+  for ( my $mod_line=0; $mod_line<$mods; $mod_line++ )
   {
-   $pid = $pm->start and next;
-   say("[$local_filename" . ":" . __LINE__ . "]Parallel Fork Mod proc $mod_line");
-   $ItemRec{CallNumber} = $new_callnumbers[$mod_line] ;
-   $UpdateItemRequest{ItemID}=$items[$mod_line] ;
-   ($result2[$mod_line],$trace2[$mod_line]) = $call2->(%UpdateItemRequest);
-   say " [$local_filename" . ":" . __LINE__ . "]Mod proc $mod_line API returned";
-   $pm->finish;
-   #$pm->finish (0, { result=>$result2[$mod_lsay " [$local_filename" . ":" . __LINE__ . "]current_line waitingine], trace=>$trace2[$mod_line],input=>$mod_line});
-   
+    $pid = $pm->start and next;
+    if ($quiet_mode==0) {
+    #say("[$local_filename" . ":" . __LINE__ . "]Parallel Fork Mod proc $mod_line");
+    }
+    $ItemRec{CallNumber} = $new_callnumbers[$mod_line] ;
+    $UpdateItemRequest{ItemID}=$items[$mod_line] ;
+  
+    ($result2[$mod_line],$trace2[$mod_line]) = $call2->(%UpdateItemRequest);
+    
+    if ($quiet_mode==0) {
+      say "[$local_filename" . ":" . __LINE__ . "]Forked modline $mod_line UpdateItem request returned";
+     }
+
+     my $MyResponseStatusCode = ($result2[$mod_line]->{UpdateItemResponse}->{ResponseStatuses}->{cho_ResponseStatus}[0]->{ResponseStatus}->{Code});
+     #say "[$local_filename" . ":" . __LINE__ . "]MyResponseStatusCode " . $MyResponseStatusCode ;
+     if ( defined($opt_g) ) {
+       $pm->finish (0,   {entry_line=>$mod_line, result=>\$MyResponseStatusCode});
+       #$pm->finish (0,  {entry_line=>$mod_line});
+       #$pm->finish (0,   {entry_line=>$mod_line, result=>\$result2[$mod_line]});
+       #$pm->finish (0,   {result=>\$result2[$mod_line], tracer=>\$trace2[$mod_line],entry_line=>$mod_line});
+     }
+     else {
+      $pm->finish;
+      } 
+  if ($quiet_mode==0) {
+   say "[$local_filename" . ":" . __LINE__ . "]mod_line waiting children...";
   }
-  say " [$local_filename" . ":" . __LINE__ . "]mod_line waiting...";
   $pm->wait_all_children;
-  say " [$local_filename" . ":" . __LINE__ . "]mod_line finished";
-  #$elapsed = tv_interval ($t0) ;
-  #say ("[$local_filename" . ":" . __LINE__ . "]Parallel Fork Call lapsed time $elapsed.");
-
-  #  print Dumper \%results;
- 
-   
-   
-   #
-   #my $MyResponseStatusCode = ($result2->{UpdateItemResponse}->{ResponseStatuses}->{cho_ResponseStatus}[0]->{ResponseStatus}->{Code});
-   #
-   #if ( (defined $MyResponseStatusCode) && ($MyResponseStatusCode == 0) )
-   # {
-   # say "[$local_filename" . ":" . __LINE__ . "]ItemAPI Success: $MyResponseStatusCode" ;
-   # }     
   
-  
+  if ( $quiet_mode==0 )   {
+    say "[$local_filename" . ":" . __LINE__ . "]mod_line finished";
+    }
+   #$elapsed = tv_interval ($t0) ;
+   #say ("[$local_filename" . ":" . __LINE__ . "]Parallel Fork Call lapsed time $elapsed.");
+  } #end for ( my $mod_line=0; $mod_line<$mods; $mod_line++ )
 
+  
      
      
---
+++ newcn.pl
@@ -0,0 +1,71 @@
+# Author:  <wblake@CB95043>
+# Created: May 17, 2022
+# Version: 0.01
+# Usage: perl [-g] [-r ] [-x] [-i] newCN.pl cnfile.csv
+# -g Debug/verbose -r read only , no update, -i in, -q quiet
+#
+#
+use strict;
+use diagnostics;
+use integer;
+use Getopt::Std;
+use Data::Dumper;
+use say;
+
+use constant NEW_CALLNUM_PREFIX => "ER" ;
+use constant OLD_CALLNUM_PREFIX=> "E";
+use constant OLD_CALLNUM_SUFFIX=> "- BEGINNING TO READ";
+
+our ($opt_g,$opt_r,$opt_x, $opt_i,$opt_q);
+getopts('grx:iq');
+
+my $local_filename=$0;
+my $prefix;
+my $author;
+my $separator;
+my $newcallnum;
+
+$local_filename =~ s/.+\\([A-z]+.pl)/$1/;
+       
+if ( defined($opt_g) ) {
+     say "[$local_filename" . ":" . __LINE__ . "]Debug Mode $opt_g." ;
+}
+
+#my $CALLNUM_PATTERN1 = qr/^(\s*ER*)\s+(([\w+\-']) *)+(-*) *BEGINNING TO READ/ ;
+my $CALLNUM_PATTERN1 = qr/^(\s*ER*\s+)([\w'\-]+)\s*(-*) *(BEGINNING TO READ)/;
+
+# Read and print first line containing column header information.
+$_=<>;
+chomp;
+my ($item_header, $bid_header,  $oldcn_header, $newcn_header) = split(/,/,$_,5);
+say "$item_header, $bid_header, $oldcn_header, $newcn_header" ;
+
+while (<>) {
+  chomp;
+  my ($item, $bid, $callnum)= split(/,/); 
+  
+  if (!defined($opt_q)) {
+      
+      say "[$local_filename" . ":" . __LINE__ . "]$_" ;
+  }
+			
+  if ( $callnum =~ $CALLNUM_PATTERN1 ) {
+
+    $prefix = $1;
+    $author = $2 ;
+    $separator = $3;
+    #Print if debug or not quiet
+    if (!defined ($opt_q) || defined($opt_g)) {
+    say "[$local_filename" . ":" . __LINE__ . "]Match E <author> - BEGINNING TO READ" ;
+    say "[$local_filename" . ":" . __LINE__ . "]Match 1 $1 2 $2 3 $3 4 $4" ;
+    say "[$local_filename" . ":" . __LINE__ . "]Match Prefix $prefix Author $author Separator $separator";
+    }
+    $newcallnum = NEW_CALLNUM_PREFIX . " " . $author;
+    say "$item,$bid,$callnum,$newcallnum";
+  }
+  #Debug print what's not matching;
+  elsif (defined($opt_g))
+  {
+        say "[$local_filename" . ":" . __LINE__ . "]$. Does Not Match $_" ;
+  }  
+}
